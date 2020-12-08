2591bee9e145eb1bc93f149a37c8be9aa9e75603
jasminesodhi <contact@jasminesodhi.com>
2020-12-09 03:33:28 +0530

    Project setup

---
+++ app/src/main/java/com/jasminesodhi/moody/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jasminesodhi.moody;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
